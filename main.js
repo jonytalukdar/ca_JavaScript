aea9dd668444bfa4c120114e6e6f6f8f3814b90a
Jony Talukdar <jonytalukdar30@gmail.com>
2020-11-11 00:38:01 -0800

    i have edit html and javascript file

--- main.js
+++ main.js
@@ -1,5 +1,55 @@
-document.querySelectorAll('.btn').forEach((item) =>{
-    item.addEventListener('click' , () =>{
-        alert('this is awesome')
-    })
-})
+// document.write()
+// console.log()
+// alert('hello world')
+
+document.write('hello world');
+console.log('hello world');
+console.log({ name: 'joney' });
+alert('hello people');
+
+// I woke up early today
+// statements - sets of instructions
+// comments - shortcut command ctrl + /
+
+console.log('hello world');
+console.log('hello people');
+console.log('hello my friend');
+console.log('hello joney');
+
+// variables - most basic building block
+// variables - store , access , modify == value
+// declare - assignment operator , assign value
+// assign value letter , modify existing
+
+let name = 'joney';
+name = 'joney talukdar'
+let age = 21;
+age = 23
+ageGrow = age + 2;
+console.log('My name is ' + name + ' and i am ' + age + ' years old');
+console.log(ageGrow);
+
+let address, zip, state;
+address = 'Dhaka , Bangladesh';
+zip = 'github.com/jonytalukdar';
+state = 12;
+console.log(address , zip , state);
+
+
+
+// can contains digits , letters , underscore , $
+// must start with letter , $ or _
+
+let random123_$ = 'random'
+console.log(random123_$);
+
+// no keyword
+// cannot start with number
+
+// let let = 'this is demo'
+// console.log(let);
+
+// case sensitive fullname vs Fullname
+
+let fullName = 'random'
+console.log(fullName)
