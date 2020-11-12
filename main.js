60130333380dd22b03f18c2545013b405205ac9e
Jony Talukdar <jonytalukdar30@gmail.com>
2020-11-11 21:50:19 -0800

    i have added some line

--- main.js
+++ main.js
@@ -1,55 +1,87 @@
-// document.write()
-// console.log()
-// alert('hello world')
+// // document.write()
+// // console.log()
+// // alert('hello world')
 
-document.write('hello world');
-console.log('hello world');
-console.log({ name: 'joney' });
-alert('hello people');
+// document.write('hello world');
+// console.log('hello world');
+// console.log({ name: 'joney' });
+// alert('hello people');
 
-// I woke up early today
-// statements - sets of instructions
-// comments - shortcut command ctrl + /
+// // I woke up early today
+// // statements - sets of instructions
+// // comments - shortcut command ctrl + /
 
-console.log('hello world');
-console.log('hello people');
-console.log('hello my friend');
-console.log('hello joney');
+// console.log('hello world');
+// console.log('hello people');
+// console.log('hello my friend');
+// console.log('hello joney');
 
-// variables - most basic building block
-// variables - store , access , modify == value
-// declare - assignment operator , assign value
-// assign value letter , modify existing
+// // variables - most basic building block
+// // variables - store , access , modify == value
+// // declare - assignment operator , assign value
+// // assign value letter , modify existing
 
-let name = 'joney';
-name = 'joney talukdar'
-let age = 21;
-age = 23
-ageGrow = age + 2;
-console.log('My name is ' + name + ' and i am ' + age + ' years old');
-console.log(ageGrow);
+// let name = 'joney';
+// name = 'joney talukdar'
+// let age = 21;
+// age = 23
+// ageGrow = age + 2;
+// console.log('My name is ' + name + ' and i am ' + age + ' years old');
+// console.log(ageGrow);
 
-let address, zip, state;
-address = 'Dhaka , Bangladesh';
-zip = 'github.com/jonytalukdar';
-state = 12;
-console.log(address , zip , state);
+// let address, zip, state;
+// address = 'Dhaka , Bangladesh';
+// zip = 'github.com/jonytalukdar';
+// state = 12;
+// console.log(address , zip , state);
 
+// // can contains digits , letters , underscore , $
+// // must start with letter , $ or _
 
+// let random123_$ = 'random'
+// console.log(random123_$);
 
-// can contains digits , letters , underscore , $
-// must start with letter , $ or _
+// // no keyword
+// // cannot start with number
 
-let random123_$ = 'random'
-console.log(random123_$);
+// // let let = 'this is demo'
+// // console.log(let);
 
-// no keyword
-// cannot start with number
+// // case sensitive fullname vs Fullname
 
-// let let = 'this is demo'
-// console.log(let);
+// let fullName = 'random'
+// console.log(fullName)
 
-// case sensitive fullname vs Fullname
+// // let vs var vs const
 
-let fullName = 'random'
-console.log(fullName)
+// // using var
+// var value = 'some value'
+// value = 'somethings is change'
+// // using let
+// let name = 'john'
+// name  = 'john doe'
+// // using const (CONSTANT) cant not re assign
+// const lastName = 'Jordar'
+// lastName = 'mone Jordar'
+
+// console.log(value);
+// console.log(name);
+// console.log(lastName);
+
+// //  ""  or ''
+
+// const name = "john's course are the best";
+// const name2 = "john's course are awesome";
+// const name3 = 'john\\ course are best';
+// console.log(name);
+// console.log(name2);
+// console.log(name3);
+
+// // strings concatenation - combine string values
+// //  `` - backticks (template strings) easier option
+
+// const name = 'john';
+// const lastName = 'shakeandbake';
+// let fullName = name + ' ' +  lastName;
+
+// console.log('Hello there your full name is ' + fullName);
